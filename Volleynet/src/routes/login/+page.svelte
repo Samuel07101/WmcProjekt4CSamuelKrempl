6a2f675e6d9dd8cb9910ae1acd52dde5fa8fcf78
Samuel <KremplS21084@sus.htl-grieskirchen.at>
2026-05-20 08:57:46 +0200

    base for login in frontend for testing later

---
+++ Volleynet/src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    const butText = $state('Login');
+    let name = $state('')
+
+    function btnLogin(){
+
+    }
+</script>
+
+
+{#if butText === 'Login'}
+    <h1>Login</h1>
+
+<input type="text" bind:innerText={name} contenteditable="true">
+
+<button onclick={btnLogin()}>{butText}</button>
+{/if}
+
