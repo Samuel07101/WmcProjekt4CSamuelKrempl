71a6ee30c706a2482dbcd77993faf91982fc2573
Samuel <KremplS21084@sus.htl-grieskirchen.at>
2026-05-20 10:22:44 +0200

    finished registration

--- Volleynet/src/routes/login/+page.svelte
+++ Volleynet/src/routes/login/+page.svelte
@@ -1,18 +1,65 @@
 <script>
-    const butText = $state('Login');
-    let name = $state('')
+     
 
-    function btnLogin(){
+    let btnText = $state('Login');
+    let swapText = $state('Registration');
+    let email = $state('');
+    let password = $state('');
 
+    let fullName = $state('');
+    let birthdate = $state('');
+    let country = $state('AT')
+    const countries = $state(['AT','DE','CH']);
+    let date = $state();
+    function loginOrRegistration(){
+        if(btnText == 'Login'){
+            const user = {email: email, password: password};
+            login(user);
+        }else{
+            const user = {
+            fullName: fullName,
+            birthdate: birthdate,
+            country: country,
+            email: email,
+            password: password
+        };
+
+        register(user);
+        }
+    }
+    function swap(){
+        const placeholder = btnText;
+        btnText = swapText;
+        swapText = placeholder;
+
+        
     }
 </script>
 
+<h1>{btnText}</h1>
 
-{#if butText === 'Login'}
-    <h1>Login</h1>
+{#if btnText === 'Login'}
+<input type="text" bind:value={email}>
 
-<input type="text" bind:innerText={name} contenteditable="true">
+<input type="password" bind:value={password}>
+{:else}
 
-<button onclick={btnLogin()}>{butText}</button>
+<input type="text" bind:value={fullName}>
+<select bind:value={country}>
+    {#each countries as ctry}
+        <option value={ctry}>
+            {ctry}
+        </option>
+    {/each}
+</select>
+
+<input type="date" bind:value={date}>
+
+<input type="text" bind:value={email}>
+
+<input type="password" bind:value={password}>
+
 {/if}
 
+<button onclick={swap}>{swapText}</button>
+<button onclick={loginOrRegistration()}>{btnText}</button>
