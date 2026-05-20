ff6cdedfea4f76f235baa0d138b57371dd8405a8
Samuel <KremplS21084@sus.htl-grieskirchen.at>
2026-05-20 09:33:17 +0200

    made fetches on frontend and finished login

---
+++ Volleynet/src/routes/index.js
@@ -0,0 +1,25 @@
+const URL = 'https://localhost:3000/'
+
+async function login(user) {
+    const response = await fetch(URL+'login', {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: user
+    });
+    const bool = await response.json()
+    return bool;
+}
+
+async function registration(user) {
+    const response = await fetch(URL+'login', {
+    method: "PUSH",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: user
+    });
+
+    return OK;
+}
